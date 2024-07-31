40019f33ca8d1a9a1c029112c0c3586a8dde2eff
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-07-31 16:17:22 +0200

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+import { Person } from "./Person.js";
+
+const person1 = new Person("awodwa", "Hendrick", 23, "blue")
+const person2 = new Person("Chelsea", "Julie", 19, "green")
+const person3 = new Person("Daniel", "John", 19, "brown")
+
+console.log(person1.firstName);
+
+console.log(person2.lastName);
+
+console.log(person3.age);
